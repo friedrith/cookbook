de8a7d43a4d0e91121ff893667dce02bdb9334ff
Thibault Friedrich <thibault.friedrich@gmail.com>
2022-10-28 17:18:33 -0400

    feat: compress the image for thumbnails

--- api/src/index.ts
+++ api/src/index.ts
@@ -16,10 +16,52 @@ recipes.routesConfig(app)
 
 export const api = functions.https.onRequest(app)
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import * as path from 'path'
+import { tmpdir } from 'os'
+import { spawn } from 'node:child_process'
+import { unlinkSync } from 'fs'
+
+// https://firebase.google.com/docs/storage/extend-with-functions
+export const generateThumbnail = functions.storage
+  .object()
+  .onFinalize(async object => {
+    console.log('generate thumbnail')
+    const fileBucket = object.bucket // The Storage bucket that contains the file.
+    const filePath = object.name || '' // File path in the bucket.
+    const contentType = object.contentType || '' // File content type.
+
+    // Exit if this is triggered on a file that is not an image.
+    if (!contentType.startsWith('image/')) {
+      return functions.logger.log('This is not an image.')
+    }
+
+    // Get the file name.
+    const fileName = path.basename(filePath)
+    // Exit if the image is already a thumbnail.
+    if (fileName.startsWith('thumb_')) {
+      return functions.logger.log('Already a Thumbnail.')
+    }
+
+    // Download file from bucket.
+    const bucket = admin.storage().bucket(fileBucket)
+    const tempFilePath = path.join(tmpdir(), fileName)
+    const metadata = {
+      contentType: contentType,
+    }
+
+    await bucket.file(filePath).download({ destination: tempFilePath })
+    functions.logger.log('Image downloaded locally to', tempFilePath)
+    // Generate a thumbnail using ImageMagick.
+    spawn('convert', [tempFilePath, '-thumbnail', '200x200>', tempFilePath])
+    functions.logger.log('Thumbnail created at', tempFilePath)
+    // We add a 'thumb_' prefix to thumbnails file name. That's where we'll upload the thumbnail.
+    const thumbFileName = `thumb_${fileName}`
+    const thumbFilePath = path.join(path.dirname(filePath), thumbFileName)
+    // Uploading the thumbnail.
+    await bucket.upload(tempFilePath, {
+      destination: thumbFilePath,
+      metadata: metadata,
+    })
+    // Once the thumbnail has been uploaded delete the local file to free up disk space.
+    return unlinkSync(tempFilePath)
+  })
