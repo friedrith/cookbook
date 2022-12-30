963582801db7e70c54e139aaacc5207b2504e8f7
Thibault Friedrich <thibault.friedrich@gmail.com>
2022-12-30 18:19:27 -0500

    doc: add stackoverflow page

--- src/utils/platforms/windows.ts
+++ src/utils/platforms/windows.ts
@@ -1,2 +1,3 @@
+// https://stackoverflow.com/questions/61868907/how-to-open-google-chrome-from-webview-via-javascript
 export const openBrowserWindow = (path: string) =>
   window.open(`intent:${process.env.REACT_APP_URL}${path}#Intent;end`)
