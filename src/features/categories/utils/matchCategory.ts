0cb0052b78ee2344e2c5bbbdc9ca70c310fdc44b
Thibault Friedrich <thibault.friedrich@gmail.com>
2023-01-31 21:14:21 -0500

    feat: ensure keywords even in caps

--- src/features/categories/utils/matchCategory.ts
+++ src/features/categories/utils/matchCategory.ts
@@ -4,7 +4,7 @@ import Category from '../Category'
 const matchCategory = (query: SearchQuery, category: Category) => {
   if (typeof query !== 'string' || !query.startsWith('#')) return false
 
-  return query.toLowerCase().substring(1) === category.name
+  return query.toLowerCase().substring(1) === category.name.toLowerCase()
 }
 
 export default matchCategory
