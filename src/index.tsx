4b16dc12a2759b51bc2ac0ae01e82d9c08022319
Thibault Friedrich <thibault.friedrich@gmail.com>
2023-05-16 08:30:20 -0400

    chore: reduce to identify bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
@@ -19,15 +20,17 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 const App = () => <div>foo</div>
 
 root.render(
-  <HelmetProvider>
-    <BrowserRouter>
-      <AuthProvider>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </AuthProvider>
-    </BrowserRouter>
-  </HelmetProvider>,
+  <React.StrictMode>
+    {/* <HelmetProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <Provider store={store}> */}
+    <App />
+    {/* </Provider>
+        </AuthProvider>
+      </BrowserRouter>
+    </HelmetProvider> */}
+  </React.StrictMode>,
 )
 
 // If you want to start measuring performance in your app, pass a function
