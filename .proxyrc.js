e2b0ff519b7879b67d9e81d719157984666097a9
Thibault Friedrich <thibault.friedrich@gmail.com>
2023-05-16 07:52:19 -0400

    chore: fix parcel build

--- .proxyrc.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-const serveStatic = require('serve-static')
-
-const assetsPath = path.join(__dirname, 'public')
-
-module.exports = function (app) {
-  app.use('/', serveStatic(assetsPath))
-}
