dda0f6c7450b3efe1f2cf55a405e9fce6271a085
Thibault Friedrich <thibault.friedrich@gmail.com>
2023-05-16 08:23:56 -0400

    chore: reduce to identify bug

--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,17 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 // eslint-disable-next-line i18next/no-literal-string
 const App = () => <div>foo</div>
 
-root.render(<App />)
+root.render(
+  <HelmetProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </AuthProvider>
+    </BrowserRouter>
+  </HelmetProvider>,
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
@@ -28,4 +38,4 @@ reportWebVitals()
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.register()
+serviceWorkerRegistration.register()
