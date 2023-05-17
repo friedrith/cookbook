d1518688742e15a5a0fa0ebbc6660b7b1fbcdb87
Thibault Friedrich <thibault.friedrich@gmail.com>
2023-05-17 06:55:37 -0400

    fix: auth provider

--- src/features/authentication/components/AuthProvider.tsx
+++ src/features/authentication/components/AuthProvider.tsx
@@ -8,6 +8,11 @@ interface Props {
 const AuthProvider: React.FC<Props> = ({ children }) => {
   const navigate = useNavigate()
 
+  console.log(
+    'process.env.REACT_APP_CLERK_PUBLISHABLE_KEY',
+    process.env.REACT_APP_CLERK_PUBLISHABLE_KEY,
+  )
+
   return (
     <ClerkProvider
       publishableKey={process.env.REACT_APP_CLERK_PUBLISHABLE_KEY ?? ''}
