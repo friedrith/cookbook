3647251c6bf72b7498245845c8f747ea86b63944
Thibault Friedrich <thibault.friedrich@gmail.com>
2023-05-17 07:48:22 -0400

    fix: page

--- src/features/authentication/components/AuthProvider.tsx
+++ src/features/authentication/components/AuthProvider.tsx
@@ -11,6 +11,7 @@ const AuthProvider: React.FC<Props> = ({ children }) => {
   console.log(
     'process.env.REACT_APP_CLERK_PUBLISHABLE_KEY',
     process.env.REACT_APP_CLERK_PUBLISHABLE_KEY,
+    process.env.REACT_APP_API_URL,
   )
 
   return (
