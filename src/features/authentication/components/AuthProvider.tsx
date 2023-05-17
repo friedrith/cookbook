14b2f775e0a5c56f27d3e712bfb7a1a141b70d66
Thibault Friedrich <thibault.friedrich@gmail.com>
2023-05-17 07:17:03 -0400

    refactor: remove ClerkProvider to be sure

--- src/features/authentication/components/AuthProvider.tsx
+++ src/features/authentication/components/AuthProvider.tsx
@@ -1,4 +1,5 @@
-import { ClerkProvider } from '@clerk/clerk-react'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// import { ClerkProvider } from '@clerk/clerk-react'
 import { useNavigate } from 'react-router-dom'
 
 interface Props {
@@ -8,19 +9,19 @@ interface Props {
 const AuthProvider: React.FC<Props> = ({ children }) => {
   const navigate = useNavigate()
 
-  console.log(
+  console.error(
     'process.env.REACT_APP_CLERK_PUBLISHABLE_KEY',
     process.env.REACT_APP_CLERK_PUBLISHABLE_KEY,
   )
 
-  return (
-    <ClerkProvider
-      publishableKey={process.env.REACT_APP_CLERK_PUBLISHABLE_KEY ?? ''}
-      navigate={to => navigate(to)}
-    >
-      {children}
-    </ClerkProvider>
-  )
+  return <>{children}</>
+
+  // return (
+  //   <ClerkProvider
+  //     publishableKey={process.env.REACT_APP_CLERK_PUBLISHABLE_KEY ?? ''}
+  //     navigate={to => navigate(to)}
+  //   ></ClerkProvider>
+  // )
 }
 
 export default AuthProvider
